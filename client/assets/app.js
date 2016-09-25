a2e87b657845fcdec8fdf3cd1f15af5d2406073e
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-24 23:11:55 -0400

    reordered some routed for readability

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,12 +1,12 @@
-var app = angular.module('app', ['ngRoute', 'ngCookies']);
+var app = angular.module('app', ['ngRoute', 'ngCookies', 'ngMaterial']);
 app.config(function($routeProvider){
 $routeProvider
-	.when('/dashboard', {
-		templateUrl: 'partials/dashboard.html',
-		controller: 'batchController'
-	})
 	.when('/', {
 		templateUrl: 'partials/stations.html',
+		controller: 'batchController'
+	})
+	.when('/dashboard', {
+		templateUrl: 'partials/dashboard.html',
 		controller: 'batchController'
 	})
 	.when('/wash', {
@@ -29,21 +29,21 @@ $routeProvider
 		templateUrl: 'partials/batch.html',
 		controller: 'batchController'
 	})
+	.when('/new_customer', {
+		templateUrl: 'partials/newcustomer.html',
+		controller: 'customerController'
+	})
 	.when('/edit_customer/:id', {
 		templateUrl: 'partials/edit_customer.html',
 		controller: 'customerController'
 	})
-	.when('/new_customer', {
-		templateUrl: 'partials/newcustomer.html',
+	.when('/customer_manage', {
+		templateUrl: 'partials/manage_customer.html',
 		controller: 'customerController'
 	})
 	.when('/new_item',  {
 		templateUrl: 'partials/newitem.html',
 		controller: 'itemController'
-	})
-	.when('/customer_manage', {
-		templateUrl: 'partials/manage_customer.html',
-		controller: 'customerController'
 	})
 	.when('/item_manage', {
 		templateUrl: 'partials/manage_items.html',
@@ -57,7 +57,7 @@ $routeProvider
 	// 	templateUrl: 'partials/login.html',
 	// 	controller: 'credentialController'
 	// })
-		.otherwise({
-			redirectTo: '/'
-		})
+	.otherwise({
+		redirectTo: '/'
 	});
+});
