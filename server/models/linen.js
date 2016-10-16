b632be5a33936b7b805dc1e0eb0afdc0c355ae25
Patrick Todd <patrick.a.todd@gmail.com>
2016-10-16 12:14:26 -0400

    patched up a few validations that were causing node to crash, comma here, semicolon there

--- server/models/linen.js
+++ server/models/linen.js
@@ -1,5 +1,5 @@
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var AdminSchema = new mongoose.Schema({
 	email: {
@@ -12,7 +12,7 @@ var AdminSchema = new mongoose.Schema({
 		type: String,
 		required: [true, "Your password must be at least 8 characters long and less than 32 characters long."]
 	}
-}, {timestamps:true})
+}, {timestamps:true});
 
 AdminSchema.pre('save', function(done){
 	if(this.isNew){
@@ -21,124 +21,124 @@ AdminSchema.pre('save', function(done){
 	bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
 });
 
-var BatchSchema = new mongoose.Schema({ 
-	status: { 
-		type: String, 
-		required: true, 
+var BatchSchema = new mongoose.Schema({
+	status: {
+		type: String,
+		required: true,
 		maxlength: 15
-	}, 
-	instructions: { 
-		type: String, 
+	},
+	instructions: {
+		type: String,
 		maxlength: 60
-	}, 
-	recieved_by: { 
-		type: String, 
+	},
+	recieved_by: {
+		type: String,
 		maxlength: 20
-	}
-	due_date: String, 
+	},
+	due_date: String,
 	_customer: {
-		type: Schema.Types.ObjectId, 
+		type: Schema.Types.ObjectId,
 		ref: "Customers"
 	},
 	order: [{
-		name: { type: String, maxlength: 20}
-		price: { type: String, maxlength: 20}
-		charge: { type: String, maxlength: 20} 
-		quantity: { type: String, maxlength: 20}
-		subtotal: { type: String, maxlength: 20}	
+		name: { type: String, maxlength: 20},
+		price: { type: String, maxlength: 20},
+		charge: { type: String, maxlength: 20},
+		quantity: { type: String, maxlength: 20},
+		subtotal: { type: String, maxlength: 20}
 	}]
-}, {timestamps: true})
+}, {timestamps: true});
 
 var ItemSchema = new mongoose.Schema({
-	name: { 
-		type: String, 
-		required: [true, "Your item must have a name."] 
+	name: {
+		type: String,
+		required: [true, "Your item must have a name."],
 		maxlength: 60,
 		trim: true,
-	}, 
-	type: { 
-		type: String, 
+	},
+	type: {
+		type: String,
 		maxlength: 20,
 		trim: true,
-	}, 
-	size: { type: String, maxlength: 20}, 
-	charge : { 
-		type: String, 
-		required: [true, "Do you plan to charge per item or per pound."]
+	},
+	size: { type: String, maxlength: 20},
+	charge : {
+		type: String,
+		required: [true, "Do you plan to charge per item or per pound."],
 		maxlength: 10,
 		trim: true,
-	},  
-	price: { 
-		type: String, 
-		required: true, 
-		maxlength: 20,
-		trim: true,
-	},  
-	created_by: { 
-		type: String, 
+	},
+	price: {
+		type: String,
 		required: true,
 		maxlength: 20,
 		trim: true,
-	}, 
+	},
+	created_by: {
+		type: String,
+		required: true,
+		maxlength: 20,
+		trim: true,
+	},
 }, {timestamps: true});
 
 var CustomerSchema = new mongoose.Schema({
-	name: { 
-		type: String, 
-		required: [true, "You must enter the company's name."] 
+	name: {
+		type: String,
+		required: [true, "You must enter the company's name."],
 		maxlength: 40,
 		trim: true,
-	}, 
-	contact_name: { 
-		type: String, 
-		required: [true, "You really want to have a person to contact for questions."]
+	},
+	contact_name: {
+		type: String,
+		required: [true, "You really want to have a person to contact for questions."],
 		maxlength: 20,
 		trim: true,
-	}, 
-	phone: { 
-		type: String, 
-		required: [true, "You must enter a phone number."] 
+	},
+	phone: {
+		type: String,
+		required: [true, "You must enter a phone number."],
 		maxlength: 20,
 		trim: true,
-	}, 
-	email: { 
-		type: String, 
-		required: [true, "We need to have a email address for billing."] 
+	},
+	email: {
+		type: String,
+		required: [true, "We need to have a email address for billing."],
 		maxlength: 40,
 		trim: true,
-	}, 
-	address: { 
-		type: String, 
-		required: [true, "You must enter a deliery or billing address."]
+	},
+	address: {
+		type: String,
+		required: [true, "You must enter a deliery or billing address."],
 		maxlength: 40
-	}, 
-	address2: { 
-		type: String, 
-		required: true, 
+	},
+	address2: {
+		type: String,
+		required: true,
 		maxlength: 40
-	},  
-	address3: { 
-		type: String, 
-		required: true, 
+	},
+	address3: {
+		type: String,
+		required: true,
 		maxlength: 40
-	}, 
-	weight_price: Number, 
-	comments: { 
-		type: String, 
+	},
+	weight_price: Number,
+	comments: {
+		type: String,
 		required: true,
 		 maxlength: 60,
 		 trim: true,
-		}, 
+		},
 }, {timestamps:true});
 
 mongoose.model('Admins', AdminSchema);
-var Admins = mongoose.model('Admins')
+var Admins = mongoose.model('Admins');
 
 mongoose.model('Batches', BatchSchema);
-var Batches = mongoose.model('Batches')
+var Batches = mongoose.model('Batches');
 
-mongoose.model('Items', ItemSchema); 
-var Items = mongoose.model('Items')
+mongoose.model('Items', ItemSchema);
+var Items = mongoose.model('Items');
 
-mongoose.model('Customers', CustomerSchema); 
-var Customers = mongoose.model('Customers')
+mongoose.model('Customers', CustomerSchema);
+var Customers = mongoose.model('Customers');
