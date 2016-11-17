775da2521ae3a0c2edd5925fc535ad97fb6ffb56
Patrick Todd <patrick.a.todd@gmail.com>
2016-11-17 12:02:04 -0500

    house cleaning and route tracing

--- client/assets/factories/itemFactory.js
+++ client/assets/factories/itemFactory.js
@@ -1,25 +1,26 @@
+/* esversion: 6 */
 app.factory('itemFactory', ['$http', function($http){
 	function itemFactory(){
 		this.create = function(item, callback){
 			$http.post('/create_item', item).then(function(results){
-				callback(results.data)
+				callback(results.data);
 			});
-		}
+		};
 		this.delete = function(id, callback){
 			$http.post('/delete_item', id).then(function(results){
 				callback(results.data);
 			});
-		}
+		};
 		this.edit = function(id, item, callback){
 			$http.post(`/edit_item/${id}`, item).then(function(results){
 					callback(results.data);
 			});
-		}
+		};
 		this.all = function(callback){
 			$http.get('/get_items').then(function(results){
 				callback(results.data);
 			});
-		}
+		};
 	}
-	return new itemFactory()	
+	return new itemFactory();
 }]);
